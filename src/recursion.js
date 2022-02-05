71ca011d7c53443a10ddb2a7bb419f2d877e510a
Blake Crawford <blake.crawford@outlook.com>
2022-02-05 10:58:05 -0800

    added sum recursion functionality

--- src/recursion.js
+++ src/recursion.js
@@ -30,6 +30,16 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  // I: an array of numbers
+  // O: the sum of each element in the array
+  // C: none
+  // E: if array is empty return zero
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array[0] + sum(array.slice(1));
+  }
+
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
